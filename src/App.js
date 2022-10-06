7cc87846c34a2478d82920a2a37925ba72252ce3
amitanshusahu <amitanshushau.official@gmail.com>
2022-10-06 20:47:11 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './Components/Header';
 import { Nav } from './Components/Nav';
 import { Work } from './Components/Work';
+import { ToolKit } from './Components/ToolKit';
 
 function App() {
   return (
     <>
     <Header/>
     <Nav/>
+    <ToolKit/>
     <Work/>
     </>
   );
